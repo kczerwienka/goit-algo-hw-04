ff633f217f3e1f9fc2245333b1e56ded2a718afc
kczerwienka <57370801+kczerwienka@users.noreply.github.com>
2024-06-02 18:43:04 +0200

    final commit

--- Task.py
+++ Task.py
@@ -1,7 +1,9 @@
 import timeit
 import numpy as np
+import matplotlib.pyplot as plt
 
-n = 10
+n = 50
+
 
 def merge_sort(lst):
 
@@ -38,6 +40,7 @@ def merge_sort(lst):
 
     return merge(merge_sort(left_half), merge_sort(right_half))
 
+
 def insertion_sort(lst):
     for i in range(1, len(lst)):
         key = lst[i]
@@ -48,8 +51,35 @@ def insertion_sort(lst):
         lst[j+1] = key
     return lst
 
-results = np.zeros((4, n))
-for i in range (1, n+1):
-    current_list = np.random.randint(0, 10**i , size = i)
 
-    timeit.timeit(merge_sort(current_list), number=1)
+results = np.zeros((4, n))
+
+for i in range (1, n+1):
+    current_list = np.random.randint(0, 10**9, size=i)
+
+
+    # Calculating execution times
+    results[0, i-1] = 10**i
+    results[1, i-1] = timeit.timeit("merge_sort(current_list)",
+                                    setup="from __main__ import merge_sort, current_list", number=10000)
+    results[2, i-1] = timeit.timeit("insertion_sort(current_list)",
+                                    setup="from __main__ import insertion_sort, current_list", number=10000)
+    results[3, i-1] = timeit.timeit("sorted(current_list)",
+                                    setup="from __main__ import insertion_sort, current_list", number=10000)
+
+# Plotting results
+fig, ax = plt.subplots()
+ax.plot(results[0, :], results[1, :], label="merge sort")
+ax.plot(results[0, :], results[2, :], label="insertion sort")
+ax.plot(results[0, :], results[3, :], label="sorted")
+
+# Setting logarithmic scale
+ax.set_xscale('log')
+
+# Adding legend and labels
+ax.legend()
+ax.set_title("Sort results")
+ax.set_xlabel("List length: 10^n")
+ax.set_ylabel("Time (s)")
+
+plt.show()
